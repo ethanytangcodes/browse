52ec764f8ec85c29f1b25ec330a43fc9bb2efc5c
ethanytangcodes <orangepea8@gmail.com>
2025-10-05 14:32:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,13 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // List of official Helios URLs
     const officialUrls = [
-        "https://ethanytangcodes.github.io/",
-        "https://helios-browser.vercel.app/",
-        "https://helios-blue.vercel.app/",
-        "https://helios-browser.rf.gd",
-        "https://helios-browser.w3spaces.com/",
-        "https://helios-browser.pages.dev/",
-        "https://helios-browser.ct.ws"
+        "https://ethanytangcodes.github.io/browse/"
     ];
 
     function normalizeUrl(url) {
